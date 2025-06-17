e4198fa825392226b86dd9b639879a7d0c6a7412
Jordy <jordy_hanno@hotmail.com>
2025-06-17 14:43:11 +0200

    Add ContactMessage resource with CRUD pages for managing contact messages

---
+++ app/Filament/Resources/ContactMessageResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactMessageResource\Pages;
+use App\Models\ContactMessage;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ContactMessageResource extends Resource
+{
+    protected static ?string $model = ContactMessage::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-inbox';
+
+    protected static ?string $navigationGroup = 'Helpdesk';
+    protected static ?string $navigationLabel = 'Messages';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('id')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->description(fn(ContactMessage $record): string => $record->email)
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('subject')
+                    ->description(fn(ContactMessage $record): string => $record->message)
+                    ->wrap()
+                    ->searchable()
+                    ->label('Message'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                //
+            ])
+            ->bulkActions([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactMessages::route('/'),
+            'create' => Pages\CreateContactMessage::route('/create'),
+            'edit' => Pages\EditContactMessage::route('/{record}/edit'),
+        ];
+    }
+}
