a41ee138e73088a15c4dbca87c95a870a548a396
Wim Lemmens <wim.lemmens@gmail.com>
2025-06-18 15:34:06 +0200

    Restored PasswordUpdate tests

--- tests/Feature/Settings/PasswordUpdateTest.php
+++ tests/Feature/Settings/PasswordUpdateTest.php
@@ -13,39 +13,39 @@ class PasswordUpdateTest extends TestCase
 {
     use RefreshDatabase;
 
-    // public function test_password_can_be_updated(): void
-    // {
-    //     $user = User::factory()->create([
-    //         'password' => Hash::make('password'),
-    //     ]);
+    public function test_password_can_be_updated(): void
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
 
-    //     $this->actingAs($user);
+        $this->actingAs($user);
 
-    //     $response = Livewire::test(Password::class)
-    //         ->set('current_password', 'password')
-    //         ->set('password', 'new-password')
-    //         ->set('password_confirmation', 'new-password')
-    //         ->call('updatePassword');
+        $response = Livewire::test(Password::class)
+            ->set('current_password', 'password')
+            ->set('password', 'new-password')
+            ->set('password_confirmation', 'new-password')
+            ->call('updatePassword');
 
-    //     $response->assertHasNoErrors();
+        $response->assertHasNoErrors();
 
-    //     $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
-    // }
+        $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
+    }
 
-    // public function test_correct_password_must_be_provided_to_update_password(): void
-    // {
-    //     $user = User::factory()->create([
-    //         'password' => Hash::make('password'),
-    //     ]);
+    public function test_correct_password_must_be_provided_to_update_password(): void
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('password'),
+        ]);
 
-    //     $this->actingAs($user);
+        $this->actingAs($user);
 
-    //     $response = Livewire::test(Password::class)
-    //         ->set('current_password', 'wrong-password')
-    //         ->set('password', 'new-password')
-    //         ->set('password_confirmation', 'new-password')
-    //         ->call('updatePassword');
+        $response = Livewire::test(Password::class)
+            ->set('current_password', 'wrong-password')
+            ->set('password', 'new-password')
+            ->set('password_confirmation', 'new-password')
+            ->call('updatePassword');
 
-    //     $response->assertHasErrors(['current_password']);
-    // }
+        $response->assertHasErrors(['current_password']);
+    }
 }
